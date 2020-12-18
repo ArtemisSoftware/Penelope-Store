2179aed7040ce7829c036378d2030fa0fcb8790a
gustavo <gustavo.isaac@gmail.com>
2020-12-18 18:38:08 +0000

    ProductDto

---
+++ app/src/main/java/com/artemissoftware/penelopestore/api/models/ProductDto.kt
@@ -0,0 +1,56 @@
+package com.artemissoftware.penelopestore.api.models
+
+import com.google.gson.annotations.SerializedName
+import java.util.*
+
+
+class ProductDto (
+
+    @SerializedName("id")
+    var id: Int,
+
+    @SerializedName("brand")
+    var brand: String,
+
+    @SerializedName("name")
+    var name: String,
+
+    @SerializedName("price")
+    var price: Double,
+
+    @SerializedName("price_sign")
+    var priceSign: String,
+
+    @SerializedName("image_link")
+    var imageLink: String,
+
+    @SerializedName("description")
+    var description: String,
+
+    @SerializedName("category")
+    var category: String,
+
+    @SerializedName("product_type")
+    var productType: String,
+
+    @SerializedName("tag_list")
+    var tags: List<String>,
+
+    @SerializedName("updated_at")
+    var updatedAt: Date,
+
+    @SerializedName("product_colors")
+    var colors: List<ProductColorsDto>
+)
+
+class ProductColorsDto (
+
+    @SerializedName("hex_value")
+    var hex: String,
+
+    @SerializedName("colour_name")
+    var colourName: String
+)
+
+
+
