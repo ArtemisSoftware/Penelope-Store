8318f4be2a6d5a56170c8ff0b40224ed1baf80cf
gustavo <gustavo.isaac@gmail.com>
2020-12-21 18:04:53 +0000

    StoreFrameFragment

---
+++ app/src/main/java/com/artemissoftware/penelopestore/ui/frame/StoreFrameFragment.kt
@@ -0,0 +1,7 @@
+package com.artemissoftware.penelopestore.ui.frame
+
+import androidx.fragment.app.Fragment
+import com.artemissoftware.penelopestore.R
+
+class StoreFrameFragment : Fragment(R.layout.fragment_store_frame){
+}
